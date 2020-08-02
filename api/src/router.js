8f011b003ff5d5805ca7ae43b9ec0d2b47984835
Shreyas Myana <shreyasmyana1997@gmail.com>
2020-08-02 11:37:20 +0530

    Workers added

--- api/src/router.js
+++ api/src/router.js
@@ -1,8 +1,11 @@
 const multer = require('multer')
 const Router = require('express').Router
+const path = require('path')
 const storage = multer.diskStorage({destination:'api/uploads/', filename :filename})
 const upload = multer({fileFilter:fileFilter,storage:storage})
 const router = Router()
+
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html')
 
 function filename(request, file, callback){
     callback(null, file.originalname)
@@ -24,5 +27,9 @@ router.post('/upload', upload.single('photo'), (request, response)=>{
     }
     response.status(201).json({success:true})
 })
+
+router.get('/photo-viewer',(request, response)=>{
+    response.sendFile(photoPath)
+} )
 
 module.exports = router
