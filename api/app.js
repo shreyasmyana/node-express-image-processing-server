6ed37dbefdbefcea5ad187ac0745a533044c5335
Shreyas Myana <shreyasmyana1997@gmail.com>
2020-08-02 11:19:11 +0530

    Second-final

--- api/app.js
+++ api/app.js
@@ -5,9 +5,9 @@ const multer = require('multer')
 const app = express()
 const router = require('./src/router')
 
-const pathToIndex = path.resolve('__dirname', '../client/index.html');
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
 app.use('/',router);
-app.use(express.static(path.resolve('__dirname','uploads')))
+app.use(express.static(path.resolve(__dirname, 'uploads')))
 app.use('/*',(request,response)=>{
     response.sendfile(pathToIndex);
 });
--- api/src/router.js
+++ api/src/router.js
@@ -9,9 +9,9 @@ function filename(request, file, callback){
 }
 
 function fileFilter(request, file, callback){
-    if(file.mimetype != 'image/png'){
+    if(file.mimetype !== 'image/png'){
         request.fileValidationError = 'Wrong file type';
-        callback(null, false , {Error: 'Wrong file type'})
+        callback(null, false, new Error('Wrong file type'))
     }
     else{
         callback(null, true)
